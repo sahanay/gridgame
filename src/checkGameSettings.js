f31a02a84f838bce64e863e62fb03fc8770f5164
Sahan Aydin <sahan_aydin@icloud.com>
2017-03-19 21:20:41 +0100

    Update comments

--- src/checkGameSettings.js
+++ src/checkGameSettings.js
@@ -13,17 +13,26 @@
   */
 
   function checkGameSettings(settings) {
+
+    // check if settings is an objects
+    // otherwise throw error
     if (typeof settings !== 'object') {
       throw new Error('Please provide the settings for the game as an object.');
     }
 
+    // check if settings contains the properties:
+    // - rows {integer}
+    // - columns {integer}
+    // - el {settings}
+    // otherwise throw error
     if (!(settings.hasOwnProperty('rows')    && typeof settings.rows    == 'number' &&
           settings.hasOwnProperty('columns') && typeof settings.columns == 'number' &&
           settings.hasOwnProperty('el')      && typeof settings.el      == 'string')) {
-
+      
       throw new Error('Please check the provided properties in your settings object.');
     }
 
+    // in any other case, return true
     return true;
   }
 
