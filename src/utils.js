e006cf028510c4b826d3c159ddf9a65176d6898d
Sahan Aydin <sahan_aydin@icloud.com>
2017-03-19 21:30:09 +0100

    Removed the tableIndexNR utility

--- src/utils.js
+++ src/utils.js
@@ -7,13 +7,9 @@ var utils = {
   id: function(id) {
     return document.getElementById(id);
   },
-  
+
   create: function(elementName) {
     return document.createElement(elementName);
-  },
-
-  tableIndexNR: function(element) {
-    return element.closest('tr').indexNumber;
   }
 
 }
